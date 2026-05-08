3c429d9486932ee1782e26e7d0c23de2dee828ae
Yashas08-RY <ryyashas05@gmail.com>
2026-05-08 14:42:06 +0530

    Added product APi'S

---
+++ backend/products/views.py
@@ -0,0 +1,26 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Product
+from .serializers import ProductSerializer
+
+
+@api_view(['GET', 'POST'])
+def product_list_create(request):
+
+    # GET PRODUCTS
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    # ADD PRODUCT
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
